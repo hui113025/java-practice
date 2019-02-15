f113f78723aac1b936d27003d662cde347307b6a
zhenghui3 <VuPz-7646>
2019-02-15 17:12:35 +0800

    switch执行顺序

--- src/main/java/com/zheng/example/TestSwitch.java
+++ src/main/java/com/zheng/example/TestSwitch.java
@@ -4,5 +4,21 @@ public class TestSwitch {
 
     public static void main(String[] args) {
         char c = 0;
+        int i=9;
+        //从上到下支持,结果是default,zero
+        switch (i) {
+            default:
+                System.out.println("default");
+                //此处无break中断继续执行
+            case 0:
+                System.out.println("zero");
+                break;
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+        }
     }
 }
